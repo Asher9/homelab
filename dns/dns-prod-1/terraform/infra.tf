113c66e2da203ff74625dd64bd6ba33589090182
Asher9 <info@prox-lab.de>
2025-06-15 13:20:23 +0200

    added dns ptr

--- dns/dns-prod-1/terraform/infra.tf
+++ dns/dns-prod-1/terraform/infra.tf
@@ -5,11 +5,25 @@ resource "dns_a_record_set" "router" {
   ttl       = 3600
 }
 
+resource "dns_ptr_record" "router_ptr" {
+  zone = "100.168.192.in-addr.arpa."
+  name = "1"
+  ptr  = "router.local.prox-lab.de."
+  ttl  = 3600
+}
+
 resource "dns_a_record_set" "aruba_switch" {
   zone      = "local.prox-lab.de."
   name      = "aruba-switch"
   addresses = ["192.168.100.2"]
   ttl       = 3600
+}
+
+resource "dns_ptr_record" "aruba_switch_ptr" {
+  zone = "100.168.192.in-addr.arpa."
+  name = "2"
+  ptr  = "aruba-switch.local.prox-lab.de."
+  ttl  = 3600
 }
 
 resource "dns_a_record_set" "aruba_ap_master" {
@@ -19,9 +33,23 @@ resource "dns_a_record_set" "aruba_ap_master" {
   ttl       = 3600
 }
 
+resource "dns_ptr_record" "aruba_ap_master_ptr" {
+  zone = "100.168.192.in-addr.arpa."
+  name = "4"
+  ptr  = "aruba-ap-master.local.prox-lab.de."
+  ttl  = 3600
+}
+
 resource "dns_a_record_set" "aruba_ap_slave" {
   zone      = "local.prox-lab.de."
   name      = "aruba-ap-slave"
   addresses = ["192.168.100.5"]
   ttl       = 3600
+}
+
+resource "dns_ptr_record" "aruba_ap_slave_ptr" {
+  zone = "100.168.192.in-addr.arpa."
+  name = "5"
+  ptr  = "aruba-ap-slave.local.prox-lab.de."
+  ttl  = 3600
 }
