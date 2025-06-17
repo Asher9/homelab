b53cbdd32198d10574f955dd041bf3c9f2eba060
Asher9 <info@prox-lab.de>
2025-06-17 19:03:55 +0200

    edit dns names

--- dns/dns-prod-1/terraform/dns_server.tf
+++ dns/dns-prod-1/terraform/dns_server.tf
@@ -1,25 +1,25 @@
-resource "dns_a_record_set" "dns_prod_1" {
+resource "dns_a_record_set" "ns1" {
   zone      = "local.prox-lab.de."
-  name      = "dns-prod-1"
+  name      = "ns1"
   addresses = ["192.168.10.253"]
   ttl       = 3600
 }
-resource "dns_ptr_record" "dns_prod_1_ptr" {
+resource "dns_ptr_record" "ns1_ptr" {
   zone = "10.168.192.in-addr.arpa."
   name = "253"
-  ptr  = "dns-prod-1.local.prox-lab.de."
+  ptr  = "ns1.local.prox-lab.de."
   ttl  = 3600
 }
 
-resource "dns_a_record_set" "dns_prod_2" {
+resource "dns_a_record_set" "ns2" {
   zone      = "local.prox-lab.de."
-  name      = "dns-prod-2"
+  name      = "ns2"
   addresses = ["192.168.10.254"]
   ttl       = 3600
 }
-resource "dns_ptr_record" "dns_prod_2_ptr" {
+resource "dns_ptr_record" "ns2_ptr" {
   zone = "10.168.192.in-addr.arpa."
   name = "254"
-  ptr  = "dns-prod-2.local.prox-lab.de."
+  ptr  = "ns2.local.prox-lab.de."
   ttl  = 3600
 }
